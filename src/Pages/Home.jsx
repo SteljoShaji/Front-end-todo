658b56353c6c2c4e53cf07a58eb02f756bf71fe9
Steljo Shaji <shajisteljo@gmail.com>
2024-08-08 15:42:17 +0530

    Your commit message

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from "react";
 import ProjectCard from "../Components/ProjectCard";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import AddProject from "../Components/AddProject";
 
-
-
-
-
-
 function Home() {
- 
   const [isLoggedIn, setLoggedIn] = useState(false);
   const [username, setUsername] = useState("");
   const [showNewComponent, setShowNewComponent] = useState(false);
- // This useEffect runs only once when the component mounts
+  const navigate = useNavigate();
 
   const handleButtonClick = () => {
     setShowNewComponent(!showNewComponent);
@@ -27,29 +21,36 @@ function Home() {
   }, []);
 
   useEffect(() => {
-    setLoggedIn(!!sessionStorage.getItem("token"));
-  }, []);
+    if (sessionStorage.getItem("token")) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+      navigate("/login"); // Redirect to login page if not logged in
+    }
+  }, [navigate]);
 
   return (
     <>
-      {isLoggedIn ? <Link to={"/"} /> : <Link to={"/login"} />}
-
-      <div className="w-screen shadow-sm mb-5 sticky top-0 bg-white">
-        <div className="flex justify-between items-center container h-16">
-          <div>
-            <h1 className="text-2xl font-semibold">
-              Welcome <span className="text-warning">{username}</span>
-            </h1>
+      {isLoggedIn  && (
+        <>
+          <div className="w-screen shadow-sm mb-5 sticky top-0 bg-white">
+            <div className="flex justify-between items-center container h-16">
+              <div>
+                <h1 className="text-2xl font-semibold">
+                  Welcome <span className="text-warning">{username}</span>
+                </h1>
+              </div>
+              <div className="p-4">
+                <button onClick={handleButtonClick} className="px-3 py-1 border">
+                  {showNewComponent ? "Close Project" : "Add Project"}
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="p-4">
-            <button onClick={handleButtonClick} className="px-3 py-1 border">
-              {showNewComponent ? "Close Project" : "Add Project"}
-            </button>
-          </div>
-        </div>
-      </div>
-      <ProjectCard/>
-      {showNewComponent && <AddProject onClose={() => setShowNewComponent(false)} />}
+          <ProjectCard />
+          {showNewComponent && <AddProject onClose={() => setShowNewComponent(false)} />}
+        </>
+      )}
     </>
   );
 }
